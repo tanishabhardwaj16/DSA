15cf9d96d39356879089697b65ec4bbc69ea8945
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-02-16 16:58:16 +0530

    Time: 3 ms (98.96%), Space: 85.1 MB (17.39%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -1,14 +1,18 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        int[] arr=new int[n];
+        int[] ans=new int[n];
         for(int[] booking : bookings){
             int first=booking[0];
             int last=booking[1];
             int seats=booking[2];
-            for(int i=first-1;i<=last-1;i++){
-                arr[i]+=seats;
+            ans[first-1]=ans[first-1]+seats;
+            if(last<n){
+                ans[last]=ans[last]-seats;
             }
         }
-        return arr;
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1]+ans[i];
+        }
+        return ans;
     }
 }
