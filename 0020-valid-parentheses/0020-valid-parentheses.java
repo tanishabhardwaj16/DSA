aceae58a28854288c9c525f44e73f6d2559fe636
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-02-25 12:07:47 +0530

    Time: 4 ms (42.19%), Space: 43.6 MB (26.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,17 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character>stack=new Stack<>();
-        for(int i=0;i<s.length();i++){
-           char ch = s.charAt(i);
-           if(ch =='(' || ch=='[' || ch=='{'){
+        for(char ch : s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
                 stack.push(ch);
-           }else{
-            if(stack.isEmpty())return false;
-            char stackElement=stack.pop();
-            if(ch ==')' && stackElement!='('){
-                return false;
-            }else if(ch==']' && stackElement!='['){
-                return false;
-            }else if(ch=='}' && stackElement!='{'){
-                return false;
+            }else{
+                if(stack.isEmpty())return false;
+                char top=stack.pop();
+                if((ch==')' && top!='(')||
+                 (ch=='}' && top!='{')
+                 || (ch==']' && top!='['))return false;
             }
-           }
         }
-        return stack.isEmpty() ? true : false;
+        return stack.isEmpty();
     }
 }
