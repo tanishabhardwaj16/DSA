817de4fccfa7e202da8fab05a73e586ae22b2a51
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-04-03 12:08:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -4,19 +4,19 @@ class Solution {
         int e=nums.length-1;
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(nums[mid]==target) {
+            if(nums[mid]==target){
                 return mid;
             }else if(nums[s]<=nums[mid]){
                 if(target>=nums[s] && target<nums[mid]){
                     e=mid-1;
-                }else{
+               }else{
                     s=mid+1;
-                }
+               }
             }else{
                 if(target<=nums[e] && target>nums[mid]){
-                    s=mid+1;
-                }else{
                     e=mid-1;
+                }else{
+                    s=mid+1;
                 }
             }
         }
