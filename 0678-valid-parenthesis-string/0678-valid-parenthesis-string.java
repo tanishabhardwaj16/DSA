c6bcd8983edf5c7c09781c8263358542ceab291f
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-05-12 18:33:54 +0530

    Time: 0 ms (100%), Space: 42.8 MB (49.26%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int low=0;
+        int high=0;
+        for(char ch : s.toCharArray()){
+            if(ch=='('){
+                low++;
+                high++;
+            }else if(ch==')'){
+                low--;
+                high--;
+            }else{
+                low--;
+                high++;
+            }
+            if(high<0) return false;
+            if(low<0) low=0;
+        }
+        return low==0;
+    }
+}
