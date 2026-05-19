94ec412c94c4acbbf04d7f89fee17d0b3bdcd5cd
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-05-19 17:55:52 +0530

    Time: 5 ms (84.39%), Space: 46.9 MB (44.34%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,46 +1,30 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj=new ArrayList<>();
+        List<List<Integer>>adj=new ArrayList<>();
+        Queue<Integer>q=new LinkedList<>();
+        int[] indegree=new int[numCourses];
+        int[] ans=new int[numCourses];
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] p:prerequisites){
-            int a=p[0];
-            int b=p[1];
-            adj.get(b).add(a);
+        for(int[] p : prerequisites){
+            int u=p[0];
+            int v=p[1];
+            adj.get(v).add(u);
+            indegree[u]++;
         }
-        int[] vis=new int[numCourses];
-        int[] pathVis=new int[numCourses];
-        Stack<Integer>s=new Stack<>();
-        int[] ans=new int[numCourses];
         int index=0;
         for(int i=0;i<numCourses;i++){
-            if(vis[i]==0){
-                if(dfs(i,adj,vis,pathVis,s)){
-                    return new int[0];
-                }
+            if(indegree[i]==0) q.add(i);
+        }
+        while(!q.isEmpty()){
+            int node=q.poll();
+            ans[index++]=node;
+            for(int nei : adj.get(node)){
+                indegree[nei]--;
+                if(indegree[nei]==0) q.add(nei);
             }
         }
-        while(!s.isEmpty()){
-            ans[index++]=s.pop();
-        }
-        return ans;
+        return index==numCourses ? ans : new int[]{};
     }
-    public boolean dfs(int node,List<List<Integer>> adj,int[] vis,int[]  pathVis,Stack<Integer>s){
-        vis[node]=1;
-        pathVis[node]=1;
-        for(int neigh:adj.get(node)){
-            if(vis[neigh]==0){
-                if(dfs(neigh,adj,vis,pathVis,s)){
-                    return true;
-                }
-            }else if(pathVis[neigh]==1){
-                return true;
-            }
-        }
-        pathVis[node]=0;
-        s.push(node);
-        return false;
-    }
-  
 }
