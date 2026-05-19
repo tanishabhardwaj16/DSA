271685cf656475978e85e825ae0345d1e9ba1b1a
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-05-19 18:19:46 +0530

    Time: 14 ms (69.61%), Space: 48.7 MB (79.24%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -5,28 +5,28 @@ class Solution {
         boolean[][] vis=new boolean[m][n];
         Queue<int[]>q=new LinkedList<>();
         int[][] dist=new int[m][n];
-        int[][] dirs={{1,0},{-1,0},{0,1},{0,-1}};
-            for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
-                    if(mat[i][j]==0){
-                        vis[i][j]=true;
-                        q.add(new int[]{i,j});
-                    }
+        int[][] dirs={{-1,0},{1,0},{0,-1},{0,1}};
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    vis[i][j]=true;
+                    q.add(new int[]{i,j});
                 }
             }
-            while(!q.isEmpty()){
-                int[] curr=q.poll();
-                int r=curr[0];
-                int c=curr[1];
-                for(int[] d:dirs){
-                    int nr=r+d[0];
-                    int nc=c+d[1];
-                    if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc]){
-                        dist[nr][nc]=dist[r][c]+1;
-                        vis[nr][nc]=true;
-                        q.add(new int[]{nr,nc});
-                    }
+        }
+        while(!q.isEmpty()){
+            int[] curr=q.poll();
+            int r=curr[0];
+            int c=curr[1];
+            for(int[] d : dirs){
+                int nr=r+d[0];
+                int nc=c+d[1];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc]){
+                    dist[nr][nc]=dist[r][c]+1;
+                    vis[nr][nc]=true;
+                    q.add(new int[]{nr,nc});
                 }
+            }
         }
         return dist;
     }
