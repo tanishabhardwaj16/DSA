1a67a116d312b28f8e8e005d65c3b15c417139a7
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-05-27 20:51:17 +0530

    Time: 0 ms (100%), Space: 47.6 MB (34.91%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -19,7 +19,7 @@ class Solution {
         if(key<root.val){
             root.left=deleteNode(root.left,key);
         }else if(key>root.val){
-            root.right=deleteNode(root.right,key);;
+            root.right=deleteNode(root.right,key);
         }else{
             if(root.left==null) return root.right;
             if(root.right==null) return root.left;
