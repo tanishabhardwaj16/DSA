7362b93d1ad127dfc25220198a8a65f2cca6018b
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-06-17 18:44:31 +0530

    Time: 15 ms (46.93%), Space: 47.6 MB (13.72%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,32 +1,31 @@
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        List<Integer> ans = new ArrayList<>();
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        int n=s.length();
+        HashMap<Character,Integer>map=new HashMap<>();
+        List<Integer>ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-
-        for (int value : map.values()) {
+        for(int value : map.values()){
             ans.add(value);
         }
         Collections.sort(ans);
         Collections.reverse(ans);
-        StringBuilder sb = new StringBuilder();
-        for (int val : ans) {
-            Character freqMatched = null;
-            for (char key : map.keySet()) {
-                if (map.get(key) == val) {
-                    freqMatched = key;
+        StringBuilder sb=new StringBuilder();
+        for(int val : ans){
+            Character freqmatched=null;
+            for(char key : map.keySet()){
+                if(map.get(key)==val){
+                    freqmatched=key;
                     break;
                 }
             }
-            if (freqMatched != null) {
-                for (int i = 0; i < val; i++) {
-                    sb.append(freqMatched);
+            if(freqmatched!=null){
+                for(int i=0;i<val;i++){
+                    sb.append(freqmatched);
                 }
-                map.remove(freqMatched);
+                map.remove(freqmatched);
             }
         }
         return sb.toString();
