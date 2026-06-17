69170333f1edca270f871e4ae10cffb4a0d3c8c0
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-06-17 23:58:22 +0530

    Time: 0 ms (100%), Space: 44.4 MB (82.48%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -10,20 +10,20 @@ class Solution {
         }
         if(idx!=-1){
             for(int j=n-1;j>=0;j--){
-                if(nums[j]>nums[idx]){
-                    swap(nums,j,idx);
-                    break;
-                }
+            if(nums[j]>nums[idx]){
+                swap(nums,j,idx);
+                break;
             }
+          }
         }
         reverse(nums,idx+1,n-1);
     }
-    private void swap(int[] nums,int i,int j){
+    public void swap(int[] nums,int i,int j){
         int temp=nums[i];
         nums[i]=nums[j];
         nums[j]=temp;
     }
-    private void reverse(int[] nums,int s,int e){
+    public void reverse(int[] nums,int s,int e){
         while(s<e){
             swap(nums,s++,e--);
         }
