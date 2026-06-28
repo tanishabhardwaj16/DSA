cff89db0694da405bf37b45a77ef8e400ff6902d
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-06-28 16:31:58 +0530

    Time: 2 ms (96.01%), Space: 48.5 MB (41.88%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -4,16 +4,10 @@ class Solution {
         int j=numbers.length-1;
         while(i<=j){
             int sum=numbers[i]+numbers[j];
-            if(sum>target){
-                j--;
-            }
-            else if(sum<target){
-                i++;
-            }
-            else {
-                return new int[]{i+1,j+1};
-            }
+            if(sum>target) j--;
+            else if(sum<target) i++;
+            else return new int[]{i+1,j+1};
         }
-        return new int[]{-1,-1};
+        return new int[]{};
     }
 }
