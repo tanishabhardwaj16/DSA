afa9bf59a113b882e1ee5ab1e590e9f4b9d2a9d3
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-07-02 11:51:26 +0530

    Time: 0 ms (100%), Space: 47.7 MB (56.71%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,10 +1,8 @@
 class Solution {
     public int trap(int[] height) {
         int n=height.length;
-        int lmax=0;
-        int rmax=0;
-        int l=0;
-        int r=n-1;
+        int l=0,r=n-1;
+        int lmax=0,rmax=0;
         int total=0;
         while(l<r){
             if(height[l]<=height[r]){
