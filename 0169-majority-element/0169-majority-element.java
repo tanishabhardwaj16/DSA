d63c753975454733c7f088fc23cbb24f52df1d3b
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-07-15 11:30:03 +0530

    Time: 1 ms (99.91%), Space: 55.5 MB (66.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,11 +4,8 @@ class Solution {
         int count=0;
         for(int i=0;i<nums.length;i++){
             if(count==0) ele=nums[i];
-            if(nums[i]==ele){
-                count++;
-            }else{
-                count--;
-            }
+            if(nums[i]==ele) count++;
+            else count--;
         }
         return ele;
     }
