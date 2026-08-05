9a105c5ace159a9e9587f55311a9cae1807e26d3
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-08-05 11:00:58 +0530

    Time: 14 ms (25.98%), Space: 46.7 MB (16.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> m1=new HashMap<>();
-        HashMap<Character,Integer> m2=new HashMap<>();
-        int n=s.length() , m=t.length();
-        if(n != m) return false;
-        for(int i=0;i<n;i++){
-            char schar=s.charAt(i);
-            if(m1.containsKey(schar)){
-                m1.put(schar,m1.getOrDefault(schar,0)+1);
-            }else{
-                m1.put(schar,0);
-            }
+        HashMap<Character,Integer>smap=new HashMap<>();
+        HashMap<Character,Integer>tmap=new HashMap<>();
+        if(s.length()!=t.length()) return false;
+        for(char c : s.toCharArray()){
+            smap.put(c,smap.getOrDefault(c,0)+1);
         }
-        for(int i=0;i<m;i++){
-            char tchar=t.charAt(i);
-            if(m2.containsKey(tchar)){
-                m2.put(tchar,m2.getOrDefault(tchar,0)+1);
-            }else{
-                m2.put(tchar,0);
-            }
+        for(char c : t.toCharArray()){
+            tmap.put(c,tmap.getOrDefault(c,0)+1);
         }
-        if(m1.equals(m2)){
-            return true;
-        }else{
-            return false;
-        }    
+        if(smap.equals(tmap)) return true;
+        return false;
     }
 }
