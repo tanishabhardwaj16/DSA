d704f90da93a5a12c8cb050b0a754818f8e71f22
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-08-05 11:31:02 +0530

    Time: 6 ms (99.23%), Space: 49.6 MB (72.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -5,9 +5,7 @@ class Solution {
             char[] ch=s.toCharArray();
             Arrays.sort(ch);
             String key=new String(ch);
-            if(!map.containsKey(key)){
-                map.put(key,new ArrayList<>());
-            }
+            if(!map.containsKey(key)) map.put(key,new ArrayList<>());
             map.get(key).add(s);
         }
         return new ArrayList<>(map.values());
