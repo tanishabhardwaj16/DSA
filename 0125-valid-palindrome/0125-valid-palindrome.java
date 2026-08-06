058ba2ed248ffdb1681f677170d18769f21df257
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-08-06 12:03:16 +0530

    Time: 4 ms (46.14%), Space: 45.3 MB (36.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int n=s.length();
-        StringBuilder result=new StringBuilder();
-        for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
-            if(ch>='a' && ch<='z' || ch>='A' && ch<='Z' || ch>='0' && ch<='9'){
-                result.append(Character.toLowerCase(ch));
+        StringBuilder sb=new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(ch>='a' && ch<='z' || ch>='0' && ch<='9' || ch>='A' && ch<='Z'){
+                sb.append(Character.toLowerCase(ch));
             }
         }
-        return result.toString().equals(result.reverse().toString());
+        return sb.toString().equals(sb.reverse().toString());
     }
 }
