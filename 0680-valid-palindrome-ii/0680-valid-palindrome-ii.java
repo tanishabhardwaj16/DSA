834aee2d19a09982bc5e3cdc01000400865a089d
tanishabhardwaj16 <158125417+tanishabhardwaj16@users.noreply.github.com>
2026-08-06 12:12:25 +0530

    Time: 5 ms (15.1%), Space: 47.9 MB (12.23%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l=0;
+        int r=s.length()-1;
+        while(l<r){
+            if(s.charAt(l)==s.charAt(r)){
+                l++;
+                r--;
+            }else{
+                return ispalindrome(s,l+1,r) || ispalindrome(s,l,r-1);
+            }
+        }
+        return true;
+    }
+    public boolean ispalindrome(String s, int l, int r){
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
